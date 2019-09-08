2fd2197086a8e5da1e118dc2fe6e30a2c8decbea
Noel Arroyo <noel.arroyo1@gmail.com>
2019-09-07 20:27:39 -0400

    add state and UI to JS file

--- app.js
+++ app.js
@@ -1,46 +1,53 @@
 function onReady() {
-  const addToDoForm = document.getElementById("addToDoForm");
-  const newToDoText = document.getElementById("newToDoText");
-  const toDoList = document.getElementById("toDoList");
   const toDos = [];
+  const addToDoForm = document.getElementById("addToDoForm");
 
   function createNewToDo() {
     const newToDoText = document.getElementById("newToDoText");
+    if (!newToDoText.value) {
+      return;
+    }
+
     toDos.push({
       title: newToDoText.value,
       complete: false
     });
     newToDoText.value = "";
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById("toDoList");
+
+    toDoList.textContent = "";
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement("li");
+      const checkbox = document.createElement("input");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerHTML = "<span>Delete</span>";
+      checkbox.type = "checkbox";
+      deleteBtn.className = "mdc-button mdc-button--raised";
+      deleteBtn.id = "delete";
+      deleteBtn.style.backgroundColor = "#FF0000";
+      deleteBtn.style.margin = "5px 15px";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+
+      deleteBtn.addEventListener("click", function() {
+        newLi.parentNode.removeChild(newLi);
+      });
+    });
   }
 
   addToDoForm.addEventListener("submit", event => {
     event.preventDefault();
     createNewToDo();
-
-    let title = newToDoText.value;
-    let newLi = document.createElement("li");
-    let checkbox = document.createElement("input");
-    let deleteBtn = document.createElement("button");
-
-    checkbox.type = "checkbox";
-
-    deleteBtn.className = "mdc-button mdc-button--raised";
-    deleteBtn.innerHTML = "<span>Delete</span>";
-    deleteBtn.id = "delete";
-    deleteBtn.style.backgroundColor = "#FF0000";
-    deleteBtn.style.margin = "5px 15px";
-
-    newLi.textContent = title;
-    newLi.className = "mdc-typography--body1";
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleteBtn);
-    toDoList.appendChild(newLi);
-
-    newToDoText.value = "";
-
-    deleteBtn.addEventListener("click", function() {
-      newLi.parentNode.removeChild(newLi);
-    });
   });
 
   renderTheUI();
